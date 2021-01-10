25d90daa158a52147d04f4d0fb6a8ba2857bec28
Garv <garv.programming@gmail.com>
2021-01-10 19:35:51 +0530

    Smile-detection complete

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import cv2 as cv
 
-# Getting the classifier for detecing the faces
+# Getting the classifiers for detecing the faces and smiles.
 face_detector = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+smile_detector = cv.CascadeClassifier('haarcascade_smile.xml')
 
 # Initiating webcam
 webcam = cv.VideoCapture(0)
@@ -20,6 +21,27 @@ while True:
         # Drawing rectangles on every face identified.      
         for x,y,w,h in faces:
             cv.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+
+            # Get the current face, to detect the smile.
+            current_face = frame[y:y+h, x:x+h]
+            grey_face = cv.cvtColor(current_face, cv.COLOR_BGR2GRAY)
+
+            smile = smile_detector.detectMultiScale(grey_face, scaleFactor = 1.7, minNeighbors = 20)
+
+            # Comment out the next follwing lines to draw rectangle around the smile.
+            # for x_,y_,w_,h_ in smile:
+            #     cv.rectangle(current_face, (x_,y_), (x_+w_,y_+h_), (0,0,255), 2)
+
+            # Indicating if a smile indicated.
+            if len(smile)>0:
+                cv.putText(
+                    img = frame, 
+                    text = 'smiling', 
+                    org = (x, y+w+40),
+                    fontFace = cv.FONT_HERSHEY_SIMPLEX,
+                    fontScale = 1,
+                    color = (255,255,255),
+                )
 
     cv.imshow('Video',frame)
 
